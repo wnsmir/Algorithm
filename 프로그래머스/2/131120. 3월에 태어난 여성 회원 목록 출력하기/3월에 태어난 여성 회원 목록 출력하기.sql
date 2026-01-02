958e3e30e538eccaef570451d1d7cb06d2f4c4a1
박준용 <junyong1102@gmail.com>
2026-01-03 01:48:57 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,5 @@
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH from MEMBER_PROFILE
+
+where month(DATE_OF_BIRTH) = 3 and TLNO is not null and GENDER = 'W'
+
+order by MEMBER_ID;
