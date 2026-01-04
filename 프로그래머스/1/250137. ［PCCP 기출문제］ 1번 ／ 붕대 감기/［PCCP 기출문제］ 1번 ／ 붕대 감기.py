729c50c2b61aa82f994d88346e4e7cec38206bbd
박준용 <junyong1102@gmail.com>
2026-01-04 11:34:31 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 4.47 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,33 +1,38 @@
 def solution(bandage, health, attacks):
-    max_health = health
-    count = 0
-    idx = 0
-    time = 0
     
-    for i in range(1, attacks[-1][0]+1):
-        time += 1
-        #해당 턴에 공격이 있다면
-        if attacks[idx][0] == time:
-            health -= attacks[idx][1]
-            idx += 1
-            count = 0
-            #공격받고 체력이 0이하로 내려가면 죽음
-            if health <= 0:
-                return -1
+    time_limit = bandage[0]
+    heal = bandage[1]
+    plus = bandage[2]
+    health_count = 0
+    cur_health = health
+    
+    # 마지막 공격턴까지 반복 진행
+    for i in range(attacks[-1][0] + 1):
+        # 이번턴에 공격이 있는가?
+        check = False
+        for attack in attacks:
+            attack_time = attack[0]
+            attack_damage = attack[1]
+            # 공격타임이 현재턴이면
+            if attack_time == i:
+                check = True
+                # 공격력만큼 체력감소
+                cur_health -= attack_damage
+                if cur_health <= 0:
+                    return -1
+                health_count = 0
             
-        #공격이 없다면
-        else:
-            #이번턴 회복
-            health += bandage[1]
-            count += 1
-            if health > max_health:
-                health = max_health
-            #count가 t라면 추가회복
-            if count == bandage[0]:
-                health += bandage[2]
-                count = 0
-                if health > max_health:
-                    health = max_health
-        print(health)
-        
-    return health
+        # 이번턴에 공격이 없었다면
+        if check == False:
+            health_count += 1
+            cur_health += heal
+            if cur_health >= health:
+                cur_health = health
+            # 최대 시간으로 회복했다면
+            if health_count == time_limit:
+                cur_health += plus
+                health_count = 0
+                if cur_health >= health:
+                    cur_health = health
+
+    return cur_health
