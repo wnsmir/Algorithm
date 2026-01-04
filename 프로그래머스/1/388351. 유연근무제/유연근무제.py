cf7a2ab2db943606c1f009bab99c36d3ba78ad16
박준용 <junyong1102@gmail.com>
2026-01-04 15:34:18 +0900

    [level 1] Title: 유연근무제, Time: 2.72 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,61 +1,55 @@
 def solution(schedules, timelogs, startday):
     answer = 0
-    limit_time = []
-    # 인덱스별로 시간제한 추가
-    for time in schedules:
-        temp = str(time)
-        r_time = temp.split(":")
-        limit_time.append(r_time)
-
-    idx = 0
-    for times in timelogs:
-        flag = True
-        day = startday
-        for t in times:
-            # 주말이면 pass
-            if day == 6 or day == 7:
-                day += 1
-                continue
-            else:
-                # 주말이 지나면 1로 초기화
-                if day == 8:
-                    day = 1
-                
-                # 출근시간안에 들어오면
-                limit = str(int(limit_time[idx][0]) + 10)
-                if len(limit) == 3:
-                    if int(limit[1]) >= 6:
-                        temp = list(limit)
-                        temp[0] = str(int(temp[0]) + 1)
-                        temp[1] = str(int(temp[1]) - 6)
-                        temp = ''.join(temp)
-                        limit = int(temp)
-                    else:
-                        limit = int(limit_time[idx][0]) + 10
-                        
-                elif len(limit) == 4:
-                    if int(limit[2]) >= 6:
-                        temp = list(limit)
-                        temp[1] = str(int(temp[1]) + 1)
-                        temp[2] = str(int(temp[2]) - 6)
-                        temp = ''.join(temp)
-                        limit = int(temp)
-                    else:
-                        limit = int(limit_time[idx][0]) + 10
-
-            
-                if limit >= t:
-                    day += 1
-                    continue
-                # 지각하면 탈락
-                else:
-                    flag = False
-                    break
-                        
-        # 7일모두 통과하면
-        if flag == True:
-            answer += 1
-        # 다음사람
-        idx += 1
+    
+    auth_time = []
+    for sc in schedules:
+        hour = sc // 100
+        minute = sc % 100 + 10
         
+        # 한시간을 넘기면
+        if minute >= 60:
+            minute -= 60
+            hour += 1
+            
+        auth_time.append((hour, minute))
+    
+    # 각 사원별 반복
+    i = 0
+    for timelog in timelogs:
+        # 제한 시간, 제한 분
+        check = 0 # check가 7이 되면 상품 get
+        
+        limit_hour = auth_time[i][0]
+        limit_min = auth_time[i][1]
+        i += 1
+        today = startday -1
+        # 한 사원의 타임라인과 리밋 비교
+        for time in timelog:
+            today += 1
+            if today > 7:
+                today %= 7
+                
+            # 오늘이 주말이면 check +1 하고 continue
+            if today == 6 or today == 7:
+                check += 1
+                continue
+                
+            cur_hour = time // 100
+            cur_min = time % 100
+            
+            # 출근시간이 제한시간보다 느리면 탈락
+            if cur_hour > limit_hour:
+                check = 0
+                continue
+            # 분 지각했을때
+            elif cur_hour == limit_hour:
+                if cur_min > limit_min:
+                    check = 0
+                    continue
+            # 다 통과하면 check += 1
+            check += 1
+        
+        if check == 7:
+            answer += 1
+
     return answer
