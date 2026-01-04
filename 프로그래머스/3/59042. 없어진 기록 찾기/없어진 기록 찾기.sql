f9da9998c8561e81537f867fc08ce65426eefe89
박준용 <junyong1102@gmail.com>
2026-01-04 17:02:30 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT o.ANIMAL_ID as ANIMAL_ID, o.name as NAME from ANIMAL_OUTS o
+left join ANIMAL_INS i on i.ANIMAL_ID = o.ANIMAL_ID
+
+where i.ANIMAL_ID is null
+
+order by ANIMAL_ID;
