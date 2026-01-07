f989c4e90ff06e9df86e865035970bf3c191a7e1
박준용 <junyong1102@gmail.com>
2026-01-07 14:45:23 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,51 +1,39 @@
 def solution(dartResult):
-    dart_count = 0
-    dart = {}
-    dart[0] = 0
-    answer = 0
-    
-    for i in range(len(dartResult)):
-        option = ''
-        # 다트기회라면
-        if dartResult[i].isdigit():
-            dart_count += 1
-            # 10일때
-            if dartResult[i+1].isdigit():
-                dart_score = int(dartResult[i] + dartResult[i+1])
-                area = area = dartResult[i+2]
-                dartResult = list(dartResult)
-                dartResult[i+1] = 'i'
-                dartResult = ''.join(dartResult)
-                if i+3 < len(dartResult) and not dartResult[i+3].isdigit():
-                    # 그 다음은 옵션
-                    option = dartResult[i+3]
-            # 한자리 수 일때
-            else:
-                dart_score = int(dartResult[i])
-                area = dartResult[i+1]
-                # 다다음이 숫자가 아니고 끝이 아니라면
-                if i+2 < len(dartResult) and not dartResult[i+2].isdigit():
-                    # 그 다음은 옵션
-                    option = dartResult[i+2]
-                
-            # area와 option으로 계산하기
-            if area == 'S':
-                dart[dart_count] = dart_score
-                
-            elif area == 'D':
-                dart[dart_count] = dart_score * dart_score
-                
-            elif area == 'T':
-                dart[dart_count] = dart_score * dart_score * dart_score
-                
-            if option == '*':
-                dart[dart_count -1] *= 2
-                dart[dart_count] *= 2
-                
-            elif option == '#':
-                dart[dart_count] *= -1
-    
-    for i in range(len(dart)):
-        answer += dart[i]
+    scores = []
+    i = 0
+    n = len(dartResult)
 
-    return answer
+    while i < n:
+        ch = dartResult[i]
+
+        # 1) 숫자 처리 (0~10)
+        if ch.isdigit():
+            # 10 처리
+            if ch == '1' and i + 1 < n and dartResult[i + 1] == '0':
+                scores.append(10)
+                i += 2
+            else:
+                scores.append(int(ch))
+                i += 1
+
+        # 2) 보너스 처리 (S/D/T) -> 방금 점수에 적용
+        elif ch in ('S', 'D', 'T'):
+            if ch == 'S':
+                scores[-1] = scores[-1] ** 1
+            elif ch == 'D':
+                scores[-1] = scores[-1] ** 2
+            else:  # 'T'
+                scores[-1] = scores[-1] ** 3
+            i += 1
+
+        # 3) 옵션 처리 (*, #) -> 방금 점수(및 이전 점수)에 적용
+        elif ch in ('*', '#'):
+            if ch == '*':
+                scores[-1] *= 2
+                if len(scores) >= 2:
+                    scores[-2] *= 2
+            else:  # '#'
+                scores[-1] *= -1
+            i += 1
+
+    return sum(scores)
