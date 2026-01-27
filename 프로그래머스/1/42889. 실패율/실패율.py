bf1a65573c9b49bf1b1fa1baea7e594abefc9884
박준용 <junyong1102@gmail.com>
2026-01-27 16:33:53 +0900

    [level 1] Title: 실패율, Time: 4302.08 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,19 +1,25 @@
+from collections import defaultdict
+
 def solution(N, stages):
+    failure = defaultdict(float)
+
+    # 현재 사람 수
+    now = len(stages)
+    # 각 스테이지 반복
+    for i in range(N):
+        # 각 반복마다 count 초기화
+        count = 0
+        for num in stages:
+            # 각 스테이지에 있는 사람 수 count
+            if (i+1) == num:
+                count += 1
+        failure[i+1] = 0.0 if now == 0 else count / now
+        now -= count
+        print(count, now)
     
-    fail = {}
-    count = 0
-    for i in range(1, N+1):
-        stage_count = 0
-        for j in range(len(stages)):
-            if stages[j] == i:
-                stage_count += 1
-        if len(stages) - count == 0:
-            fail[i] = 0
-        else:
-            failure = stage_count/(len(stages) - count)
-            fail[i] = failure
-            count += stage_count
+    sorted_items = sorted(failure.items(), key=lambda kv: (-kv[1], kv[0]))
+    
+    answer = [stage for stage, rate in sorted_items]
+
+    return answer
         
-    keys_sorted = [k for k, v in sorted(fail.items(), key=lambda x: x[1], reverse=True)]
-        
-    return keys_sorted
