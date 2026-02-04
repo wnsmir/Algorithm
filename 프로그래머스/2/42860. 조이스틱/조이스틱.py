904a76039d7145ef75522108a5d81bf12b2e4253
박준용 <junyong1102@gmail.com>
2026-02-04 19:56:09 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,23 +1,43 @@
 def solution(name):
-    n = len(name)
-    
-    #알파벳조절을 위한 이동횟수
-    char_count = 0
-    for char in name:
-        char_count += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
-    
-    #커서 이동 횟수
-    min_move = 987654321
+    na = ''
+    for i in range(len(name)):
+        na += "A"
+
+    # 마지막으로 A가 아닌 수의 위치
+    count = 0
+    a_count = 0
+    for ch in name:
+        if ch != "A":
+            diff = ord(ch) - ord("A")
+            count += 1
+
+            if a_count > 0:
+                count += a_count
+                a_count = 0
+        else:
+            a_count += 1
+
+    f_answer = 0
+    # 먼저 상하값만 따로 더해주기
+    for ch in name:
+        if ch != "A":
+            diff = ord(ch) - ord("A")
+            f_answer += min(diff, 26 - diff)
+
+    n = len(name)  # ✅ n이 없어서 문법/NameError 발생하던 부분 수정
+
+    min_min_root = float('inf')
+
     for i in range(n):
-        next = i + 1
-        A_count = 0
-        while next < n and name[next] == 'A':
-            next += 1
-            A_count += 1
-        move1 = i *2 + (n -next)
-        move2 = (n -next) *2 + i
-        min_move = min(min_move, move1, move2)
-    
-    answer = char_count + min_move
-    
-    return answer
+        j = i + 1
+        while j < n and name[j] == 'A':
+            j += 1
+
+        no_change = n - 1
+        change1 = 2 * i + (n - j)
+        change2 = i + 2 * (n - j)
+
+        min_root = min(no_change, change1, change2)
+        min_min_root = min(min_min_root, min_root)
+
+    return min_min_root + f_answer
